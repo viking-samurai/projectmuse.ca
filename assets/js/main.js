451332acb0cd0eb4e92ec7472d490eb990382f2e
viking-samurai <40673005+viking-samurai@users.noreply.github.com>
2023-09-12 14:11:27 -0600

    --Fresh build

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,3 @@
-//Additional JavaScript requirements
-//Back to top hidden button
-// Get the button
-let mybutton = document.getElementById("topButton");
-				
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-	mybutton.style.display = "flex";
-  } else {
-	mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-
-
 /*
 	Dopetrope by HTML5 UP
 	html5up.net | @ajlkn
