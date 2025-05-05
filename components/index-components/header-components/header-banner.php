66206b26579976beb0dd67ec645ad53f84ac3d5c
viking-samurai <40673005+viking-samurai@users.noreply.github.com>
2025-05-04 20:02:49 -0600

    ..

--- components/index-components/header-components/header-banner.php
+++ components/index-components/header-components/header-banner.php
@@ -2,8 +2,8 @@
     <header>
         <h2>Need a Muse?</h2>
         <p class="blog-description-paragraph">From beautifully designed websites and web applications, 
-            to data-driven growth strategies and search engine optimization, we provide the expert web 
+            to data-driven growth strategies and search engine optimization, I provide the expert web 
             development and business intelligence services you need to lead with your best foot forward.</p>
-        <p class="blog-description-paragraph"> Explore our services below.</p>
+        <p class="blog-description-paragraph"> Explore my services below.</p>
     </header>
 </section>
--- components/page-components/services-components/services-sideBar.php
+++ components/page-components/services-components/services-sideBar.php
@@ -1,29 +1,28 @@
 <section class="box">
 	<header>
-		<h3 style="margin-top: 5%;">About the Author</h3>
+		<h3 style="margin-top: 5%;">About the Developer</h3>
 	</header>
-	<p class="">Ryan holds a Master of Education degree from the University of Calgary, 
-		specializing in 'Teaching and Learning Languages' and 'Designing Technology-rich, Collaborative 
-		Learning Environments'.  He coaches literacy and personal development for all ages, specializing 
-		in digital literacy, web development, and business fundamentals.</p>
+	<p class="">Ryan has been coding and programming since 2015.  He enjoys working in vanilla JavaScript, 
+		as well as its most popular stack, the MERN stack.  He has been a writer since high school, and enjoys 
+		blending writing styles from across his travels and careers into his websites and applications.  Ryan 
+		continues to expand and improve as a full-stack developer, having started offering websites and 
+		applications written in a blend of PHP with the MERN stack.  PHP is a popular back-end language that 
+		greatly improves website loading times as well as back-end functionality.
+	</p>
 	<footer>
 		<a href="https://viking-samurai.github.io/ryansResume/" target="blank" class="button alt">See More</a>
 	</footer>
 </section>
 <section class="box">
 	<header>
-		<h3>All publications</h3>
+		<h3>About my tech stack</h3>
 	</header>
-	<p class="">I believe in the power of knowledge, so everything I write is free to be 
-		shared.  Please, feel free to browse my works below and enjoy.</p>
-	<p class="">Please don't forget to mention me if you like an idea that I wrote.  Plagiarism 
-		is easy, but shout-outs and fist bumps are pretty easy, too.</p>
-	<ul class="divided">
-		<li><a href="../../pages/article-pages/persistence-page.php">Persistence throughout the Journey</a></li>
-		<li><a href="../../pages/article-pages/creativity-page.php">Developing your Creative Self</a></li>
-		<li><a href="../../pages/article-pages/grinder-page.php">The Grinder</a></li>
-		<li><a href="../../pages/article-pages/truthiness-page.php">The Economics of Truthiness</a></li>
-	</ul>
+	<p class="">The MERN stack, written in a PHP framework, is a modern technology stack with a twist.  The MERN 
+		stack is an adaptable tech stack that works well for all web applications, but especially well for applications 
+		where the owner expects substantial user traffic.  PHP, on the other hand, is a classic back-end language, 
+		noted for its security, speed, and greater synergy with modern SEO and data manipulation.  Websites built 
+		in this style enjoy both speed and scalability, growing effortlessly with your business.
+	</p>
 	<footer>
 		<a href="https://viking-samurai.github.io/ryansResume/" target="blank" class="button alt">See More</a>
 	</footer>
