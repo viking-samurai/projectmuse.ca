a13badf8d5f26c7411d3dac5a780a938e3d59f59
viking-samurai <40673005+viking-samurai@users.noreply.github.com>
2025-05-06 15:41:24 -0600

    ..

--- components/page-components/second-page-footer-components/second-page-footer-row-columns/right-footer-column.php
+++ components/page-components/second-page-footer-components/second-page-footer-row-columns/right-footer-column.php
@@ -2,9 +2,9 @@
 	<header>
 		<h2>Thanks for visiting!</h2>
 	</header>
-	<a href="#" class="image featured"><img class="footer-image-index" src="images/secondaryBackground.png" alt="" /></a>
+	<a href="#" class="image featured"><img class="footer-image-index" src="../../images/secondaryBackground.png" alt="" /></a>
 	<p>
-		<a href="index.php">Project Muse</a> is a Calgary-based company dedicated to making people's 
+		<a href="../../index.php">Project Muse</a> is a Calgary-based company dedicated to making people's 
 		lives easier, while making the Internet a more beautiful place.  If you have a creative idea that you want help 
 		developing, <a href="mailto:info@projectmuse.ca">reach out</a>!</p>
 </section>
---
+++ components/page-components/second-page-footer-components/second-page-footer-row.php
@@ -0,0 +1,8 @@
+<div class="row">
+	<div class="col-8 col-12-medium">
+		<?php include 'second-page-footer-row-columns/left-footer-column.php' ?>
+	</div>
+	<div class="col-4 col-12-medium">
+		<?php include 'second-page-footer-row-columns/right-footer-column.php' ?>
+	</div>
+</div>
