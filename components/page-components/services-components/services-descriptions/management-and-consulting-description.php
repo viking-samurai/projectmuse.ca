6c992452afaf4ac14c26a2c9656347210a99f1a2
viking-samurai <40673005+viking-samurai@users.noreply.github.com>
2025-05-06 16:59:12 -0600

    ..

--- components/page-components/services-components/services-descriptions/management-and-consulting-description.php
+++ components/page-components/services-components/services-descriptions/management-and-consulting-description.php
@@ -6,21 +6,22 @@
 	</header>
 	<h4 class="main-offers-paragraph"><em>Need a hand with that...?</em></h4><br />
 	<p class="main-offers-paragraph">
-		Digital and Internet technologies are some of the fastest growing and most complex fields of our times, 
-		and with the rise of artificial intelligece and other machine learning tools taking over our offices 
-		and workspaces, there is no end in sight!  As the owner of <a href="../../index.php">Project Muse</a>, 
-		a digital tools and services provider, I take my role as a leader in digital technologies very seriously, 
-		and I have a number of management and consulting packages based around teaching and guiding businesses 
-		into full-stack web development with ease.  No matter how simple a website or application your business 
-		requires, implementing anything of a web-based nature at scale is a complex process that can be done poorly,
-		and lead to disastrous consequences as a result.
+		Modern websites and web applications are some of the most useful, albeit complex technological devices 
+		of our times.  The rise of artificial intelligece and other machine learning tools is further complicating 
+		the industry by making these websites ever more complex, allowing web developers to leverage modern tools 
+		to build out fantastic new concepts.  As the owner of <a href="../../index.php">Project Muse</a>, a digital 
+		tools and services provider, I take my role as a leader in digital technologies very seriously, and I have 
+		a number of management and consulting packages designed to teach and guide businesses into full-stack web 
+		development with ease.  No matter how simple a website or web application your business requires, implementing 
+		anything of a web-based nature at scale is a complex process that can be done poorly, and often leads to 
+		disastrous consequences as a result.
 	</p>
 	<p class="main-offers-paragraph">
-		Product management and consulting packages based around websites and web applications designed and developed 
-		by Project Muse, are built to succeed.  If you, or the people that you intend to use a piece of technology 
-		that I built don't understand how to use it to it's fullest capacity, then <a href="../../index.php#footer">
-		contact me</a>, and together we will build the perfect management or consulting package to guarantee that 
-		your project rolls out successfully.
+		Management and consulting packages based around my websites and web applications are built to succeed.  
+		If you, or any the people that you intend to use a website or web application that I built don't understand 
+		how to use it to its fullest capacity, then <a href="../../index.php#footer">contact me</a>, and together 
+		we will build the perfect management or consulting package to guarantee that your new website or web 
+		application rolls-out successfully!
 	</p>
 	<section>
 		<header>
