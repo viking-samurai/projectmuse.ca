0c65f4fd37e2d60200de1a0d65cea639387258df
viking-samurai <40673005+viking-samurai@users.noreply.github.com>
2025-05-11 08:15:57 -0600

    ..

--- components/index-components/header-components/services-section/services-section-footer.php
+++ components/index-components/header-components/services-section/services-section-footer.php
@@ -1,6 +1,6 @@
 <footer>
     <ul class="actions">
-        <li><a href="index.php#contact-section" class="button large">Get Started</a></li>
-        <li><a href="pages/services-pages/custom_Built-creations.php" class="button alt large">Learn More</a></li>
+        <li><a href="pages/services-pages/custom_Built-creations.php" class="button large">Get Started</a></li>
+        <li><a href="pages/services-pages/management-and-consulting.php" class="button alt large">Learn More</a></li>
     </ul>
 </footer>
