bfc1de8b9bccf04bec1b76673b158604a32fc751
viking-samurai <40673005+viking-samurai@users.noreply.github.com>
2025-07-07 10:17:59 -0600

    ..

--- components/page-components/second-page-footer-components/second-page-footer-row-columns/right-footer-column.php
+++ components/page-components/second-page-footer-components/second-page-footer-row-columns/right-footer-column.php
@@ -3,7 +3,7 @@
 		<h2>Thanks for visiting!</h2>
 	</header>
 	<a href="#" class="image featured"><img class="footer-image-index" src="../../images/secondaryBackground.png" alt="" /></a>
-	<p><a href="../../index.php">Project Muse</a> is a software development firm based out of Calgary, Alberta, that 
-		specializes in healthy business planning through strategic, full-stack web development partnerships.  If you 
-		have a creative idea that you want help planning and developing, <a href="mailto:info@projectmuse.ca">reach out</a>!</p>
+	<p><a href="../../index.php">Project Muse</a> is a software development firm based out of Calgary, that specializes 
+	in healthy business planning through strategic, full-stack web development partnerships.  If you have a creative 
+	idea that you want help planning and developing, <a href="mailto:info@projectmuse.ca">reach out</a>!</p>
 </section>
