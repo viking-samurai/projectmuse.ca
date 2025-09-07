c1172682e85de8d91a20186ba7e20abe9159da35
viking-samurai <40673005+viking-samurai@users.noreply.github.com>
2025-09-07 08:44:35 -0600

    ..Start portfolio repair.

--- components/index-components/footer-components/footer-row-columns/right-footer-column.php
+++ components/index-components/footer-components/footer-row-columns/right-footer-column.php
@@ -4,6 +4,6 @@
 	</header>
 	<a href="#" class="image featured"><img class="footer-image-index" src="images/secondaryBackground.png" alt="" /></a>
 	<p><a href="index.php">Project Muse</a> is a software development firm based out of Calgary, Alberta, that specializes 
-	in healthy business planning through strategic, full-stack web development partnerships.  If you have a creative idea 
-	that you want help planning, developing, and launching <a href="mailto:info@projectmuse.ca">reach out</a>!</p>
+	in forming strategic, full-stack web development partnerships.  If you have a creative idea that you want help planning, 
+	developing, and launching, please, <a href="mailto:info@projectmuse.ca">reach out</a>!</p>
 </section>
