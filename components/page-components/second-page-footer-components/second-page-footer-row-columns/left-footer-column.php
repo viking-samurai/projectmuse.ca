63069ef3f910e5e922df3bc0c419e3e9373196ad
viking-samurai <40673005+viking-samurai@users.noreply.github.com>
2025-10-24 12:43:03 -0600

    ..

--- components/page-components/second-page-footer-components/second-page-footer-row-columns/left-footer-column.php
+++ components/page-components/second-page-footer-components/second-page-footer-row-columns/left-footer-column.php
@@ -6,9 +6,9 @@
 		<li>
 			<h3>Social</h3>
 			<ul class="social">
-				<li style="padding: 0;"><a class="icon brands fa-linkedin-in" target="_blank" href="https://www.linkedin.com/in/ryan-degen-shaman-at-large"></a></li>
-				<li style="padding: 0;"><a class="icon brands fa-discord" target="_blank" href="https://discord.gg/GG2ffDph4e"></a></li>
-				<li style="padding: 0;"><a class="icon brands fa-github" target="_blank" href="https://github.com/viking-samurai/viking-samurai/blob/main/README.md"></a></li>
+				<li style="padding: 0;"><a class="icon brands fa-linkedin-in" target="blank" href="https://www.linkedin.com/in/ryan-degen-shaman-at-large"></a></li>
+				<li style="padding: 0;"><a class="icon brands fa-discord" target="blank" href="https://discord.gg/GG2ffDph4e"></a></li>
+				<li style="padding: 0;"><a class="icon brands fa-github" target="blank" href="https://github.com/viking-samurai/viking-samurai/blob/main/README.md"></a></li>
 			</ul>
 		</li>
 		<li>
