834a9abdfa629d38a7840a5fc48288de6b9e4954
viking-samurai <40673005+viking-samurai@users.noreply.github.com>
2026-03-17 06:35:21 -0600

    ..Widgetbot checkup.

--- components/index-components/header-components/header-logo.php
+++ components/index-components/header-components/header-logo.php
@@ -1,2 +1,2 @@
 <a href="index.php"><h1>Project Muse</h1></a><br>
-<h4>Full-stack Machine Shop and Brand Laboritory,<br />for Businesses and Professionals</h4>
+<h4>Full-stack Machine Shop and Brand Laboritory, for Businesses and Professionals</h4>
